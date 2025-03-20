ba8c92693abe0e23d15d2100c6f4b70140934967
zaenarifputra <zaenarifputraainurdin@gamil.com>
2025-03-21 00:28:54 +0700

    menambahkanclassSayaTubeVideo”

---
+++ SayaTubeVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tpmodul6_2311104049
+{
+    using System;
+
+    public class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999); // ID 5 digit
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID Video: {id}");
+            Console.WriteLine($"Judul Video: {title}");
+            Console.WriteLine($"Jumlah Play: {playCount}");
+        }
+    }
+}
